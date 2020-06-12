2d16337fc082ddf57746fc96a8b3ebb8148a0bf2
Walafi02 <walafif@yahoo.com>
2020-06-12 23:43:31 +0000

    Updates

---
+++ precache-manifest.2b2724adfd9801128cf82cd7f8a9b375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99694aa350afd0708d6e4c4607e71a83",
+    "url": "/uti-do-cabelo/index.html"
+  },
+  {
+    "revision": "497aff7caecbcba2ace0",
+    "url": "/uti-do-cabelo/static/css/main.ca4c7346.chunk.css"
+  },
+  {
+    "revision": "de709c4abc5937acdc38",
+    "url": "/uti-do-cabelo/static/js/2.7987403a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uti-do-cabelo/static/js/2.7987403a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497aff7caecbcba2ace0",
+    "url": "/uti-do-cabelo/static/js/main.5f8004fe.chunk.js"
+  },
+  {
+    "revision": "d8bf52e0930f0e7e4592",
+    "url": "/uti-do-cabelo/static/js/runtime-main.44f32df5.js"
+  }
+]);
