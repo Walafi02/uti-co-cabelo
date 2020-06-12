053be42add0ad809e5e5ca3e25e00f8904ec38dd
Walafi02 <walafif@yahoo.com>
2020-06-12 16:58:34 +0000

    implementando politicas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div className="app">
+      <h1>Políticas de Privacidade</h1>
+
+      <p>Por este instrumento eletrônico, WF DESENVOLVIMENTO, pessoa física com sede na cidade de Ocara - CE, informa a sua POLÍTICA DE PRIVACIDADE, conforme as disposições abaixo discriminadas:</p>
+
+      <h3>Noções Gerais:</h3>
+
+      <p>Este instrumento traz a política de privacidade dos serviços prestados pelo aplicativo UTI do Cabelo, bem como os deveres e responsabilidade do cliente e provedor.</p>
+
+      <h3>Cliente:</h3>
+
+      <p>É de total responsabilidade do cliente informar seus dados verídicos e o uso de imagens apropriadas para a aplicação, cabendo ao mesmo ser responsabilizado com exclusão de sua conta do sistema;</p>
+
+      <h3>Provedor</h3>
+
+      <p>Cabe ao provedor manter o sigilo de todas a informações dos cliente e averiguar a autenticidade das mesmas</p>
     </div>
   );
-}
+
 
 export default App;
